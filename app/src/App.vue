eb5595fd01fb7488b087c12184a572adbd42eefb
luizvelasco <luizvelasco@gmail.com>
2023-11-11 09:37:29 -0300

    components

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,20 @@
 <template>
+<div>
+  <TheHeader />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
   }
 }
 </script>
